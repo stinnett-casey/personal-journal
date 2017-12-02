73ddd707b6083dd7e501178168ebf385fe7af980
Casey Stinnett <stinnett.casey@gmail.com>
2017-12-01 17:10:29 -0700

    updated jquery to 3 and initialized best_in_place fields

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
 //= require best_in_place
 //= require turbolinks
 //= require materialize-sprockets
